80e9cb9d227c28f89ccf9a59ed88cdf050bc2d4a
Mohadese nayeb <98701574+mohinayeb@users.noreply.github.com>
2022-02-04 22:18:04 +0330

    upload my CV

---
+++ assets/js/data.js
@@ -0,0 +1,266 @@
+HomeText([
+    {
+        "title":"Mohadese nayebzade",
+        "name":"Mohadese nayebzade",
+        "profilePic":"assets/img/avatar.jpg",
+        "location": "Islamic Republic of Iran",
+        "birthday": "March 1th, 1999",
+        "email":"Nayebzademohade3@gmail.com",
+        "about":"I have Master degree of <strong>Artificial Intelligence</strong> in computer and electrical department at the Semnan University, and I'm doing my research underneath cover of supervisor <strong><a target='_blank' href='https://scholar.google.com/citations?user=AM9LzzgAAAAJ' title='Dr. Farzin Yaghmaie'>Dr. Farzin Yaghmaee</a></strong>.<br/> My main areas of interest are Artificial intelligence (AI), Machine Learning (ML), Natural Language Processing (NLP), Image Memorability. <br /> I like working on open source projects."
+    }
+]);
+Links([
+    {
+        "href":"https://github.com/amirshnll",
+        "icon":"fab fa-github",
+    },
+    {
+        "href":"https://www.slideshare.net/AmirShokri2/",
+        "icon":"fab fa-slideshare",
+    },
+    {
+        "href":"https://www.linkedin.com/in/amirshokri/",
+        "icon":"fab fa-linkedin",
+    },
+    {
+        "href":"https://www.youtube.com/channel/UCwoOSpsYCRpvqI0SM0PmzdA",
+        "icon":"fab fa-youtube",
+    },
+    {
+        "href":"https://www.kaggle.com/amirshnll",
+        "icon":"fab fa-kaggle",
+    },
+    {
+        "href":"https://scholar.google.com/citations?user=VRbi9YUAAAAJ",
+        "icon":"fal fa-graduation-cap",
+    },
+    {
+        "href":"https://orcid.org/0000-0003-4014-7982",
+        "icon":"fab fa-orcid",
+    },
+    {
+        "href":"https://anchor.fm/datatalk",
+        "icon":"fas fa-podcast",
+    },
+    {
+        "href":"https://ashokri.com",
+        "icon":"fab fa-wordpress",
+    }
+]);
+Skill([
+    {
+        "title":"<strong>- Tools & Technologies</strong>",
+    },
+    {
+        "title":"PHP",
+    },
+    {
+        "title":"Java",
+    },
+    {
+        "title":"Codeigniter",
+    },
+    {
+        "title":"Laravel",
+    },
+    {
+        "title":"Matlab",
+    },
+    {
+        "title":"WordPress",
+    },
+    {
+        "title":"python",
+    },
+    {
+        "title":"TensorFlow",
+    },
+    {
+        "title":"Linux",
+    },
+    {
+        "title":"C & C++",
+    },
+    {
+        "title":"Git",
+    },
+    {
+        "title":"CSS",
+    },
+    {
+        "title":"HTML",
+    },
+    {
+        "title":"MySQL",
+    },
+    {
+        "title":"JavaScript",
+    },
+    {
+        "title":"Django",
+    },
+    {
+        "title":"JSON",
+    },
+    {
+        "title":"XML",
+    },
+    {
+        "title":"<strong>- Industry Knowledge</strong>",
+    },
+    {
+        "title":"Machine Learning",
+    },
+    {
+        "title":"Front-end Development",
+    },
+    {
+        "title":"Agile Methodologies",
+    },
+    {
+        "title":"Object-Oriented Programming (OOP)",
+    },
+    {
+        "title":"Search Engine Optimization (SEO)",
+    },
+]);
+Language([
+    {
+        "title":"English",
+
+    },
+    {
+        "title":"Persian (Native)",
+
+    },
+]);
+interest([
+    {
+        "title":"Text mining"
+    },
+    {
+        "title":"Image Processing"
+    },
+    {
+        "title":"Image Memorability"
+    },
+    {
+        "title":"Machine Learning (ML)"
+    },
+    {
+        "title":"Natural Language Processing (NLP)"
+    }
+]);
+Experience([
+    {
+        "title":"Teaching Assistant (part-time)",
+        "at":"<a href='https://semnan.ac.ir/' title='Semnan University'>Semnan University</a>",
+        "des": "Image processing Course, Department of Electrical and Computer <br /> Machine learning Course, Department of Electrical and Computer <br /> Computer Architecture Course, Department of Electrical and Computer",
+        "startDate":"November 2020",
+        "endDate":"May 2022 (1 yr 7 mo)",
+    },
+    {
+        "title":"PHP Developer (full-time)",
+        "at":"<a href='https://chenco.ir/en' title='Cheshmenor'>Cheshmenor</a>",
+        "des": "",
+        "startDate":"January 2019",
+        "endDate":"August 2019 (8 mo)",
+    },
+    {
+        "title":"IT Technician (internship)",
+        "at":"<a href='https://damavandwaters.com/?lang=en' title='Damavand Mineral Co'>Damavand Mineral Co</a>",
+        "des": "",
+        "startDate":"September 2018",
+        "endDate":"December 2018 (4 mo)",
+    },
+    {
+        "title":"Web Developer (internship)",
+        "at":"<a href='https://www.parsonline.com/' title='ParsOnline Group'>ParsOnline Group</a>",
+        "des": "",
+        "startDate":"June 2015",
+        "endDate":"August 2015  (3 mo)",
+    },
+    {
+        "title":"PHP Developer (full-time)",
+        "at":"<a href='http://www.spiraldesign.org/' title='Spiral Design Center'>Spiral Design Center</a>",
+        "des": "",
+        "startDate":"September 2014",
+        "endDate":"January 2015  (5 mo)",
+    }
+]);
+Education([
+    {
+        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Semnan_University' title='Semnan University'>Semnan University</a>",
+        "study":"Artificial Intelligence and Robotics",
+        "grade":"Master's degree, average : 17.69 of 20.00",
+        "endDate":"2019 - 2022",
+    },
+    {
+        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Shamsipour_Technical_College' title='Shamsipour Technical and Vocational University'>Shamsipour Technical and Vocational University</a>",
+        "study":"Computer Software Technology",
+        "grade":"Bachelor's degree, average : 18.13 of 20.00",
+        "endDate":"2017 - 2019",
+    },
+    {
+        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Enghel%C4%81b-e_Esl%C4%81mi_Technical_College' title='Enghelāb-e Eslāmi Technical College'>Enghelāb-e Eslāmi Technical College</a>",
+        "study":"Computer Software Engineering",
+        "grade":"Associate's degree, average : 17.06 of 20.00",
+        "endDate":"2014 - 2017",
+    },
+    {
+        "title":"Dr-Hessabi Highschool",
+        "study":"Master of Computer Applications (MCA)",
+        "grade":"Diploma, average : 17.63 of 20.00",
+        "endDate":"2011 - 2014",
+    }
+]);
+Project([
+    {
+        "title":"camelCase.ir",
+        "link":"https://camelcase.ir",
+        "href":"https://camelcase.ir",
+        "des":"<span class='fal fa-diamond'></span> Educational Blog (Programming & AI), <span class='fal fa-crown'></span> Alexa Rank : Top 3000 top website in Iran, <span class='fal fa-globe'></span> 1100 Free Blog Post"
+    },
+    {
+        "title":"Persian Swear Words Dataset <span class='fal fa-star'></span> 155 star",
+        "link":"https://github.com/amirshnll/Persian-Swear-Words",
+        "href":"https://github.com/amirshnll/Persian-Swear-Words",
+        "des":"List of Persian Swears, DOI: <strong>10.34740/kaggle/dsv/2094967</strong>"
+    },
+    {
+        "title":"right to be forgotten (RTBF) Directory <span class='fal fa-star'></span> 112 star",
+        "link":"https://rtbf.ir",
+        "href":"https://rtbf.ir",
+        "des":"A directory of direct links to delete your account from iranian web services. | github repository : <strong><a href='https://github.com/amirshnll/rtbf.ir' title='amirshnll/rtbf.ir'>amirshnll/rtbf.ir</a></strong>"
+    },
+    {
+        "title":"Datalk Podcast (Data + [Ta] + lk) - Persian Podcast",
+        "link":"https://anchor.fm/datatalk",
+        "href":"https://anchor.fm/datatalk",
+        "des":"Subjects: Machine Learning, Data Science, Data privacy, Data Security"
+    },
+]);
+Hobby([
+    {
+        "title":"<span class='fal fa-car'></span> Driving",
+    },
+    {
+        "title":"<span class='fal fa-microphone-alt'></span> Podcast Recording"
+    },
+    {
+        "title":"<span class='fal fa-pen-nib'></span> Blogging",
+    },
+    {
+        "title":"<span class='fal fa-swimmer'></span> Swimming",
+    },
+]);
+Article([
+    /*{
+        "title":"",
+        "href":"",
+        "link":"",
+        "des":"",
+        "time":"",
+        "at":""
+    },*/
+]);
